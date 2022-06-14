85b9b54b3aac7bda58bfe5c2197a4e381d6d4860
Ivan <106931923+GubaRabotaga@users.noreply.github.com>
2022-06-14 10:36:31 +0300

    Add files via upload

--- src/hooks/useFreeTasks.js
+++ src/hooks/useFreeTasks.js
@@ -8,69 +8,21 @@ export default function () {
         "Et quas placeat ducimus. Mollitia laboriosam inventore blanditiis rerum eius eaque labore sequi. Id consequatur fugit molestiae sunt iste. Tempore qui omnis amet asperiores. Voluptates eos qui dolor qui est quis ut ut.…",
       id: 1,
       difficulty: 1,
+      creator:"Admin"  ,
+      standard:"5h",
+      progress: "0%",
+      Uphere: false
     },
     {
       title: "JS",
       description:
         "Et quas placeat ducimus. Mollitia laboriosam inventore blanditiis rerum eius eaque labore sequi. Id consequatur fugit molestiae sunt iste. Tempore qui omnis amet asperiores. Voluptates eos qui dolor qui est quis ut ut.…",
       id: 2,
-      difficulty: 2,
-    },
-    {
-      title: "JS",
-      description:
-        "Et quas placeat ducimus. Mollitia laboriosam inventore blanditiis rerum eius eaque labore sequi. Id consequatur fugit molestiae sunt iste. Tempore qui omnis amet asperiores. Voluptates eos qui dolor qui est quis ut ut.…",
-      id: 3,
-      difficulty: 3,
-    },
-    {
-      title: "JS",
-      description:
-        "Et quas placeat ducimus. Mollitia laboriosam inventore blanditiis rerum eius eaque labore sequi. Id consequatur fugit molestiae sunt iste. Tempore qui omnis amet asperiores. Voluptates eos qui dolor qui est quis ut ut.…",
-      id: 4,
-      difficulty: 4,
-    },
-    {
-      title: "JS",
-      description:
-        "Et quas placeat ducimus. Mollitia laboriosam inventore blanditiis rerum eius eaque labore sequi. Id consequatur fugit molestiae sunt iste. Tempore qui omnis amet asperiores. Voluptates eos qui dolor qui est quis ut ut.…",
-      id: 5,
-      difficulty: 5,
-    },
-    {
-      title: "JS",
-      description:
-        "Et quas placeat ducimus. Mollitia laboriosam inventore blanditiis rerum eius eaque labore sequi. Id consequatur fugit molestiae sunt iste. Tempore qui omnis amet asperiores. Voluptates eos qui dolor qui est quis ut ut.…",
-      id: 6,
-      difficulty: 6,
-    },
-    {
-      title: "JS",
-      description:
-        "Et quas placeat ducimus. Mollitia laboriosam inventore blanditiis rerum eius eaque labore sequi. Id consequatur fugit molestiae sunt iste. Tempore qui omnis amet asperiores. Voluptates eos qui dolor qui est quis ut ut.…",
-      id: 7,
-      difficulty: 7,
-    },
-    {
-      title: "JS",
-      description:
-        "Et quas placeat ducimus. Mollitia laboriosam inventore blanditiis rerum eius eaque labore sequi. Id consequatur fugit molestiae sunt iste. Tempore qui omnis amet asperiores. Voluptates eos qui dolor qui est quis ut ut.…",
-      id: 8,
-      difficulty: 8,
-    },
-    {
-      title: "JS",
-      description:
-        "Et quas placeat ducimus. Mollitia laboriosam inventore blanditiis rerum eius eaque labore sequi. Id consequatur fugit molestiae sunt iste. Tempore qui omnis amet asperiores. Voluptates eos qui dolor qui est quis ut ut.…",
-      id: 9,
-      difficulty: 9,
-    },
-    {
-      title: "JS",
-      description:
-        "Et quas placeat ducimus. Mollitia laboriosam inventore blanditiis rerum eius eaque labore sequi. Id consequatur fugit molestiae sunt iste. Tempore qui omnis amet asperiores. Voluptates eos qui dolor qui est quis ut ut.…",
-      id: 10,
       difficulty: 10,
+      creator:"User",
+      standard:"10h",
+      progress: "0%",
+      Uphere: false
     },
   ]);
   return {
